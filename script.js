2f56223d96835732b4e43856c8dab6e173534bc0
Brandon Tripp <brandongtripp@gmail.com>
2020-06-28 19:52:40 -0600

    picture in picture feature

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+//Prompt to select media strem, pass to video element and then play
+async function selectMediaStream() {
+   try {
+      const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+      videoElement.srcObject = mediaStream;
+      videoElement.onloadedmetadata = () => {
+         videoElement.play();
+      }
+   } catch(error) {
+      console.log('whoop, error here:', error);
+   }
+}
+
+
+button.addEventListener('click', async () => {
+   button.disabled = true;
+   await videoElement.requestPictureInPicture();
+   button.disabled = false;
+});
+// On load
+
+selectMediaStream();
